1d7d33fc8a644eaa10ca87703ac4a7bd0c9d9d8d
vishalicious213 <vishalicious213@gmail.com>
2024-01-22 11:24:45 -0800

    index.js: formatted starter code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
-const { chromium } = require("playwright");
+const { chromium } = require("playwright")
 
 async function saveHackerNewsArticles() {
-  // launch browser
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+    // launch browser
+    const browser = await chromium.launch({ headless: false })
+    const context = await browser.newContext()
+    const page = await context.newPage()
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com");
+    // go to Hacker News
+    await page.goto("https://news.ycombinator.com")
 }
 
 (async () => {
-  await saveHackerNewsArticles();
+    await saveHackerNewsArticles();
 })();
